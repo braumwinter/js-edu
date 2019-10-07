d6ad59c5a31d9733ee577ff06de3e3cfd490f1dd
Kate <braumwinter@gmail.com>
2019-10-07 15:48:52 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -5,10 +5,24 @@
  * @returns number of weeks needed for finish education
  */
 module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+  focus = 'family',
+  knowsProgramming = true,
+  config = {
+    family: 4
+  }
+) {
+  let full_time = 1300;
+  let week = 0;
+
+  if (knowsProgramming) {
+    full_time -= 500;
+  }
+
+  for (let key in config) {
+    if (key === focus) {
+      week = Math.ceil(full_time / config[key]);
+    }
+  }
+
+  return week;
+};
